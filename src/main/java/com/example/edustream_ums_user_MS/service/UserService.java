e3f8bc9c550f6ada8fda60674a6d958c5d3a925f
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 11:34:31 +0530

    feat: add get user by username method contract

--- src/main/java/com/example/edustream_ums_user_MS/service/UserService.java
+++ src/main/java/com/example/edustream_ums_user_MS/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.edustream_ums_user_MS.service;
 
+import com.example.edustream_ums_user_MS.dto.requestDTO.GetUserByUsernameRequestDTO;
 import com.example.edustream_ums_user_MS.dto.requestDTO.UserCreateRequestDTO;
 import com.example.edustream_ums_user_MS.dto.responseDTO.UserCreateResponseDTO;
+import com.example.edustream_ums_user_MS.dto.responseDTO.UserResponseDTO;
 
 public interface UserService {
 
     UserCreateResponseDTO createUser(UserCreateRequestDTO userCreateRequestDTO);
+
+    UserResponseDTO getByUsername(GetUserByUsernameRequestDTO getUserByUsernameRequestDTO);
 }
