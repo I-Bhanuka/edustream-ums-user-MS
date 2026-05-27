14afbfe3a86924a0c8d7c71b9befc13fb78f167a
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-27 10:11:36 +0530

    feat: add UserCreateRequestDTO for user creation

---
+++ src/main/java/com/example/edustream_ums_user_MS/dto/requestDTO/UserCreateRequestDTO.java
@@ -0,0 +1,31 @@
+package com.example.edustream_ums_user_MS.dto.requestDTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+@AllArgsConstructor
+@NullMarked
+@Builder
+@ToString
+public class UserCreateRequestDTO {
+
+    @NotNull(message = "Username cannot be null")
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotNull(message = "Password cannot be null")
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+    @NotNull(message = "Role cannot be null")
+    @NotBlank(message = "Role cannot be blank")
+    private String role;
+}
