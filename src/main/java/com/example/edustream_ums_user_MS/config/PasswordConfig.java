38bb84198e12fd99ef3b318600fb97ff496d1edf
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-29 08:58:17 +0530

    feat: add a Password configuration file as BCryptPasswordEncoder as a bean

---
+++ src/main/java/com/example/edustream_ums_user_MS/config/PasswordConfig.java
@@ -0,0 +1,16 @@
+package com.example.edustream_ums_user_MS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
